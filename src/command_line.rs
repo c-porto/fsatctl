1b8f00bb2aced05583cf905b2939d214d547bd77
Carlos Augusto Porto Freitas <carlos.portof@hotmail.com>
2024-08-03 17:52:59 -0300

    Change dependency versions

--- src/command_line.rs
+++ src/command_line.rs
@@ -1,7 +1,7 @@
 use clap::Parser;
 
 #[derive(Parser, Debug)]
-#[command(version, about, long_about)]
+#[command(version, about, long_about = None)]
 struct Args {
     /// Sets verbose logging
     #[arg(short, long)]
