a67885fc801b114c2a3925eaac975e6b7c3763b9
Carlos Augusto Porto Freitas <carlos.portof@hotmail.com>
2024-09-17 20:10:23 -0300

    style(format): Add .clang-format to project

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
-int main(int argc, char **argv) { 
-    while(argc--)
+int main(int argc, char** argv) {
+    while (argc--)
         std::cout << argv[argc] << std::endl;
-    return 0; 
+    return 0;
 }
